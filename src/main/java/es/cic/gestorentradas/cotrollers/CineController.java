9aac0e4b567720ad52800ed9d3b4bf09d91b0383
CrisRockandRolla <ccdelamo@gmail.com>
2023-07-27 11:58:54 +0200

    Cambio metodo Get de CineController por post para crear el Cine

--- src/main/java/es/cic/gestorentradas/cotrollers/CineController.java
+++ src/main/java/es/cic/gestorentradas/cotrollers/CineController.java
@@ -1,27 +1,23 @@
 package es.cic.gestorentradas.cotrollers;
 
-import es.cic.gestorentradas.dto.CineDto;
 import es.cic.gestorentradas.service.CineServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cine")
+//@RequestMapping("/cine")
 public class CineController {
     @Autowired
     private CineServiceImp cineServiceImp;
 
-    @GetMapping
-    public CineDto crear() {
-        return cineServiceImp.cargarCine();
+    @PostMapping
+    @RequestMapping("/cine")
+    public ResponseEntity crear() {
+        return ResponseEntity.ok(cineServiceImp.cargarCine());
     }
-
-//
-//    public ResponseEntity obtenerInfoCine() { //Me devuelve el Json
-//        return ResponseEntity.ok(cineServiceImp.obtenerInfoCine());
-//    }
 
 
 }
